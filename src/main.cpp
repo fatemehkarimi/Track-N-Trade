d858642bedff50995413d15a632228a4c6ff7ff0
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-09-03 12:46:43 +0430

    fix crash when reading screen size

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <QScreen>
 #include <QApplication>
 #include <settings/settings_window.h>
 
 int main(int argc, char* argv[]) {
-    Settings::Window window_settings(0.5, 0.5);
     QApplication app(argc, argv);
+    Settings::Window window_settings(1, 1);
     return app.exec();
 }
--- src/settings/settings_window.cpp
+++ src/settings/settings_window.cpp
@@ -1,16 +1,15 @@
 #include <QScreen>
-#include <QGuiApplication>
-
+#include <QApplication>
 #include "settings_window.h"
 
 Settings::Window::Window(double width_ratio, double height_ratio) {
     w_ratio = width_ratio;
     h_ratio = height_ratio;
 
-    QScreen* screen = QGuiApplication::primaryScreen();
-    QRect screenGeo = screen->geometry();
-    int height = (int)screenGeo.height() * h_ratio;
-    int width = (int)screenGeo.width() * w_ratio;
+    QScreen* screen = (static_cast<QApplication*>(QApplication::instance()))
+                            ->screens()[0];
+    int height = screen->size().height() * h_ratio;
+    int width = screen->size().width() * w_ratio;
     size = QSize(width, height);
 }
 
